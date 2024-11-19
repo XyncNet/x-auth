a712777dfd6d6af63dfc70ec8b08ec4d9955e3e7
Mike <mixartemev@gmail.com>
2024-11-19 11:53:29 +0300

    refresh working

--- x_auth/middleware.py
+++ x_auth/middleware.py
@@ -2,8 +2,9 @@ from starlette.types import ASGIApp, Receive, Scope, Send, Message
 
 
 class AuthRefreshMiddleware:
-    def __init__(self, app: ASGIApp) -> None:
+    def __init__(self, app: ASGIApp, domain: str = ".xync.net") -> None:
         self.app = app
+        self.domain = domain
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         if scope["type"] != "http":
@@ -11,7 +12,7 @@ class AuthRefreshMiddleware:
 
         async def send_wrapper(msg: Message) -> None:
             if msg["type"] == "http.response.start" and (tok := scope.pop("tok")):
-                ck = f"access_token={tok}; SameSite=None; Secure; Path=/; Domain=.xync.net"
+                ck = f"access_token={tok}; SameSite=None; Secure; Path=/; Domain={self.domain}"
                 msg["headers"].append((b"set-cookie", ck.encode()))
 
             await send(msg)
